741d1715d9793c3bdbf9afc50ef0b6414936c0d6
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-11-09 18:33:44 +0900

    chore: setup rspec

--- web/config/application.rb
+++ web/config/application.rb
@@ -36,7 +36,8 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
