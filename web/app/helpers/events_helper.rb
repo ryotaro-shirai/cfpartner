2231bba62050eb05a8d0da1af6410f0515eb55ad
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-12-22 17:00:09 +0900

    style: Polish UI for top page

--- web/app/helpers/events_helper.rb
+++ web/app/helpers/events_helper.rb
@@ -1,2 +1,12 @@
 module EventsHelper
+  STATUS_BADGES = {
+    "before_call" => { label: "CfP募集前", tone: "badge-upcoming" },
+    "now_on_call" => { label: "CfP募集中", tone: "badge-open" },
+    "end_of_call" => { label: "CfP募集終了", tone: "badge-closed" },
+    "end_of_event" => { label: "イベント終了", tone: "badge-done" },
+  }.freeze
+
+  def cfp_status_badge(event)
+    STATUS_BADGES.fetch(event.cfp_status) { { label: event.cfp_status, tone: "badge-neutral" } }
+  end
 end
