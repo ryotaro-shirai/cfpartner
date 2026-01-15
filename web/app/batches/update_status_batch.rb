f089abc7dae99ab098069d7ab2bb60a574677fc9
ryotaro-shirai <ryotaro.shirai@giftee.co>
2026-01-15 20:03:03 +0900

    fix: use event ids because possibility race conditions

--- web/app/batches/update_status_batch.rb
+++ web/app/batches/update_status_batch.rb
@@ -27,25 +27,25 @@ class UpdateStatusBatch < Batch
     def update_status
       current_time = Time.current
       ActiveRecord::Base.transaction do
-        target_no_information_events_to_before = Event.where(cfp_status: :no_information).where(cfp_start_at: current_time..)
-        target_no_information_events_to_now = Event.where(cfp_status: :no_information).where(cfp_start_at: ..current_time)
-        target_before_call_events = Event.where(cfp_status: :before_call, cfp_start_at: ..current_time)
-        target_now_on_call_events = Event.where(cfp_status: :now_on_call, cfp_end_at: ..current_time)
-        target_end_of_call_events = Event.where(cfp_status: :end_of_call, event_end_at: ..current_time)
+        target_no_information_events_to_before_ids = Event.where(cfp_status: :no_information).where(cfp_start_at: current_time..).pluck(:id)
+        target_no_information_events_to_now_ids = Event.where(cfp_status: :no_information).where(cfp_start_at: ..current_time).pluck(:id)
+        target_before_call_events_ids = Event.where(cfp_status: :before_call, cfp_start_at: ..current_time).pluck(:id)
+        target_now_on_call_events_ids = Event.where(cfp_status: :now_on_call, cfp_end_at: ..current_time).pluck(:id)
+        target_end_of_call_events_ids = Event.where(cfp_status: :end_of_call, event_end_at: ..current_time).pluck(:id)
 
-        count_target_no_information_events_to_before = target_no_information_events_to_before.update_all(cfp_status: :before_call)
+        count_target_no_information_events_to_before = Event.where(id: target_no_information_events_to_before_ids).update_all(cfp_status: :before_call)
         @logger.info "情報なし → CfP募集前：#{count_target_no_information_events_to_before}件"
 
-        count_target_no_information_events_to_now = target_no_information_events_to_now.update_all(cfp_status: :now_on_call)
+        count_target_no_information_events_to_now = Event.where(id: target_no_information_events_to_now_ids).update_all(cfp_status: :now_on_call)
         @logger.info "情報なし → CfP募集中：#{count_target_no_information_events_to_now}件"
 
-        count_target_before_call_events = target_before_call_events.update_all(cfp_status: :now_on_call)
+        count_target_before_call_events = Event.where(id: target_before_call_events_ids).update_all(cfp_status: :now_on_call)
         @logger.info "CfP募集前 → CfP募集中：#{count_target_before_call_events}件"
 
-        count_target_now_on_call_events = target_now_on_call_events.update_all(cfp_status: :end_of_call)
+        count_target_now_on_call_events = Event.where(id: target_now_on_call_events_ids).update_all(cfp_status: :end_of_call)
         @logger.info "CfP募集中 → CfP募集終了：#{count_target_now_on_call_events}件"
 
-        count_target_end_of_call_events = target_end_of_call_events.update_all(cfp_status: :end_of_event)
+        count_target_end_of_call_events = Event.where(id: target_end_of_call_events_ids).update_all(cfp_status: :end_of_event)
         @logger.info "CfP募集終了 → イベント終了：#{count_target_end_of_call_events}件"
       end
     end
