42ae1e1b958c74feaac9024aaa84bb1ebc20e3f9
ryotaro-shirai <ryotaro.shirai@giftee.co>
2026-03-16 21:51:31 +0900

    refactor: follow MVC pattern by fetching events in controller

--- web/app/controllers/admin/talk_recruitments_controller.rb
+++ web/app/controllers/admin/talk_recruitments_controller.rb
@@ -1,6 +1,7 @@
 class Admin::TalkRecruitmentsController < AdminController
   def new
     @talk_recruitment = TalkRecruitment.new
+    @events = Event.all
   end
 
   def create
@@ -9,6 +10,7 @@ class Admin::TalkRecruitmentsController < AdminController
     if @talk_recruitment.save
       redirect_to new_admin_talk_recruitment_path, notice: "CfPを作成しました"
     else
+      @events = Event.all
       render :new, status: :unprocessable_content
     end
   end
