bb1978f7cf0c4d83c41eb72ad8feb704acdb5098
ryotaro-shirai <ryotaro.shirai@giftee.co>
2026-03-21 10:36:08 +0900

    refactor: unify instance variable initialization

--- web/app/controllers/admin/talk_recruitments_controller.rb
+++ web/app/controllers/admin/talk_recruitments_controller.rb
@@ -1,7 +1,7 @@
 class Admin::TalkRecruitmentsController < AdminController
   def new
     @talk_recruitment = TalkRecruitment.new
-    @events = Event.all
+    @events = fetch_events
   end
 
   def create
@@ -10,12 +10,16 @@ class Admin::TalkRecruitmentsController < AdminController
     if @talk_recruitment.save
       redirect_to new_admin_talk_recruitment_path, notice: "CfPを作成しました"
     else
-      @events = Event.all
+      @events = fetch_events
       render :new, status: :unprocessable_content
     end
   end
 
   private
+    def fetch_events
+      Event.order(:id)
+    end
+
     def talk_recruitment_params
       params.expect(talk_recruitment: [ :title, :site_url, :start_at, :end_at, :talk_type, :event_id ])
     end
